5603f2d42572809d16a147284ed5013e40012537
Eduardo Mafezoli <edmalfizeo@gmail.com>
2024-09-07 23:12:42 -0300

    Updating js

--- script.js
+++ script.js
@@ -1,8 +1,43 @@
-// Seleciona os elementos do formulário.
-const amount = document.getElementById('amount');
+const form = document.querySelector("form")
+const amount = document.getElementById("amount")
+const expense = document.getElementById("expense")
+const category = document.getElementById("category")
 
+// Captura o evento de input para formatar o valor
 amount.oninput = () => {
-    let value = amount.value.replace(/\D/g, '');
+    // Obtém o valor atual do input e remove os caracteres não numéricos
+    let value = amount.value.replace(/\D/g, "")
 
-    amount.value = value;
+    // Transformar o valor em centatos (exemplo: 150/100 = 1.5 é equivalente a R$ 1,50)
+    value = Number(value) / 100
+
+    // Atualiza o valor do input
+    amount.value = formatCurrencyBRL(value)
+}
+
+function formatCurrencyBRL(value) {
+    // Formata o valor no padrão BRL (Real Brasileiro)
+    value = value.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    })
+
+    // Retorna o valor formatado 
+    return value
+}
+
+// Captura o evento de submit do formulário para obter os valores 
+form.onsubmit = (event) => {
+    // Previne o comportamento padrão de recarregar a página
+    event.preventDefault()
+
+    // Cria um objeto com os detalhes da nova despesa
+    const newExpense = {
+        id: new Date().getTime(),
+        expense: expense.value,
+        category_id: category.value,
+        category_name: category.options[category.selectedIndex].text,
+        amount: amount.value,
+        created_at: new Date(),
+    }
 }
